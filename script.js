d8323f3ee4aa248337f978c194b6fb9201142d2f
Artur <archy8191@gmail.com>
2026-04-03 00:31:55 +0300

    feat: add back to top button

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ const revealOnScroll = () => {
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
+
+const backToTopButton = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add("show");
+    }   else {
+        backToTopButton.classList.remove("show");
+    }
+});
+
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
